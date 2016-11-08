313238e9dbafddc16f249df63fe4c25f90bc93fe
Madalina <cristina.madalina.dragan@gmail.com>
2016-11-08 17:49:43 +0100

    added backend mock

--- Documents/NetBeansProjects/backend_mock_customer/src/main/java/backendMock/DummyCustomerBackend.java
+++ Documents/NetBeansProjects/backend_mock_customer/src/main/java/backendMock/DummyCustomerBackend.java
@@ -6,7 +6,7 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
+ 
 public class DummyCustomerBackend { //should implement the interface from the contract
 
     private DepartureDetail departureDetail;
